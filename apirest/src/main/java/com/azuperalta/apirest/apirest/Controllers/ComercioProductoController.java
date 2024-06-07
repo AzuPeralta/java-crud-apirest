b236776791a36a1b81418c893df3566fcc313fb5
Peralta, Mariela <marielajperalta@gmail.com>
2024-06-07 00:03:12 -0300

    Agrego detalles a la respuesta del metodo DELETE

--- apirest/src/main/java/com/azuperalta/apirest/apirest/Controllers/ComercioProductoController.java
+++ apirest/src/main/java/com/azuperalta/apirest/apirest/Controllers/ComercioProductoController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.azuperalta.apirest.apirest.Entities.Comercio;
 import com.azuperalta.apirest.apirest.Entities.ComercioProducto;
+import com.azuperalta.apirest.apirest.Entities.Producto;
 import com.azuperalta.apirest.apirest.Services.ComercioProductoService;
 
 @RestController
@@ -24,15 +26,22 @@ public class ComercioProductoController {
     public ComercioProducto getRelacionPorId(@PathVariable Long id) {
         return comercioProductoService.obtenerRelacionPorId(id);
     }
-   @PutMapping("/{id}")
+
+    @PutMapping("/{id}")
     public ComercioProducto updateRelacion(@PathVariable Long id, @RequestBody ComercioProducto detallesNuevaRelacion) {
         return comercioProductoService.actualizarRelacion(id, detallesNuevaRelacion);
     }
 
     @DeleteMapping("/{id}")
     public String deleteRelacion(@PathVariable Long id) {
+
+        ComercioProducto relacion = comercioProductoService.obtenerRelacionPorId(id);
+        Producto producto = relacion.getProducto();
+        Comercio comercio = relacion.getComercio();
+
         comercioProductoService.eliminarRelacion(id);
-        return "No es posible borrar relación con el ID: " + id + " ya que antes debes borrar el produto que está asociado al comercio.";
+        return "No es posible borrar relación con el ID: " + id + " ya que antes debes borrar el producto "
+                + producto.getNombre() + " que está asociado al comercio " + comercio.getNombreComercio() + ".";
     }
 
     @PostMapping("/{comercioId}/productos/{productoId}")
